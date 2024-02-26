90022e65d4b00729ae135789d052392153327390
Ece Tavasli <ece.tavasli@okta.com>
2024-02-26 12:10:27 +0100

    chore: make lua script more readable

--- lib/take_elevated.lua
+++ lib/take_elevated.lua
@@ -7,19 +7,22 @@ local erl_tokens_per_ms           = tonumber(ARGV[6])
 local erl_bucket_size             = tonumber(ARGV[7])
 local erl_activation_period_mins  = tonumber(ARGV[8])
 
--- check if we should use erl
+-- the key to use for pulling last bucket state from redis
+local lastBucketStateKey = KEYS[1]
+
+-- the key for checking in redis if elevated rate limits (erl) were activated earlier
 local erlKey = KEYS[2]
 local is_erl_activated = redis.call('EXISTS', erlKey)
 
-local current_time = redis.call('TIME')
-local current_timestamp_ms = current_time[1] * 1000 + current_time[2] / 1000
-
-local current = redis.pcall('HMGET', KEYS[1], 'd', 'r')
-
+-- get current bucket state
+local current = redis.pcall('HMGET', lastBucketStateKey, 'd', 'r')
 if current.err ~= nil then
     current = {}
 end
 
+-- get current time from redis, to be used in new bucket size calculations later
+local current_time = redis.call('TIME')
+local current_timestamp_ms = current_time[1] * 1000 + current_time[2] / 1000
 
 local function calculateNewBucketContent(current, tokens_per_ms, bucket_size, current_timestamp_ms)
     if current[1] and tokens_per_ms then
@@ -38,38 +41,38 @@ local function calculateNewBucketContent(current, tokens_per_ms, bucket_size, cu
     end
 end
 
-local new_content = calculateNewBucketContent(current, tokens_per_ms, bucket_size, current_timestamp_ms)
-local enough_tokens = new_content >= tokens_to_take
-
+-- Enable verbatim replication to ensure redis sends script's source code to all masters
+-- managing the sharded database in a clustered deployment.
 -- https://redis.io/docs/interact/programmability/eval-intro/#:~:text=scripts%20debugger.-,Script%20replication,-In%20standalone%20deployments
 redis.replicate_commands()
 
-if enough_tokens and is_erl_activated == 0 then
+-- calculate new bucket content
+local new_content = calculateNewBucketContent(current, tokens_per_ms, bucket_size, current_timestamp_ms)
+local enough_tokens = new_content >= tokens_to_take
+if enough_tokens and is_erl_activated==0 then
     new_content = math.min(new_content - tokens_to_take, bucket_size)
 else
-    -- use erl
+    -- calculate new bucket content based on elevated rate limits
     new_content = calculateNewBucketContent(current, erl_tokens_per_ms, erl_bucket_size, current_timestamp_ms)
-
     -- if activating erl for first time, refill the bucket with the old bucket size
     if is_erl_activated == 0 then
         new_content = erl_bucket_size - bucket_size
+        -- save erl state
         redis.call('SET', erlKey, '1')
         redis.call('EXPIRE', erlKey, erl_activation_period_mins * 60)
         is_erl_activated = 1 -- this will be returned to the caller, so we should set it
     end
-
     enough_tokens = new_content >= tokens_to_take
     if enough_tokens then
         new_content = math.min(new_content - tokens_to_take, erl_bucket_size)
     end
 end
 
-
-
-redis.call('HMSET', KEYS[1],
+-- save bucket state
+redis.call('HMSET', lastBucketStateKey,
             'd', current_timestamp_ms,
             'r', new_content)
-redis.call('EXPIRE', KEYS[1], ttl)
+redis.call('EXPIRE', lastBucketStateKey, ttl)
 
 local reset_ms = 0
 if drip_interval > 0 then
